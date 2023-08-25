7ab989356657ba386d889e4ae0b2e7ffd31277f7
Drownie <chaoscrafty01@gmail.com>
2023-08-25 19:49:56 +0700

    create task item widget and app container widget

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,12 @@ class AppHome extends StatefulWidget {
 class _AppHomeState extends State<AppHome> {
   var selectedIndex = 0;
 
+  List<String> tasksName = <String>[
+    "Task 1",
+    "Task 2",
+    "Task 3",
+  ];
+
   void _changeIndex(int newIndex) {
     setState(() {
       selectedIndex = newIndex;
@@ -40,11 +46,75 @@ class _AppHomeState extends State<AppHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hello World - $selectedIndex'),
+      body: AppContainer(selectedIndex: selectedIndex, tasksName: tasksName),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        foregroundColor: Theme.of(context).colorScheme.onPrimary,
+        tooltip: 'Add new task',
+        child: const Icon(Icons.add),
       ),
       bottomNavigationBar: AppBottomNav(
         changePage: _changeIndex,
+      ),
+    );
+  }
+}
+
+class AppContainer extends StatelessWidget {
+  const AppContainer(
+      {super.key, required this.selectedIndex, required this.tasksName});
+
+  final int selectedIndex;
+  final List<String> tasksName;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Theme.of(context).colorScheme.primaryContainer,
+      child: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: .8),
+        children: tasksName.map((String taskName) {
+          return TaskItem(taskName: taskName);
+        }).toList(),
+      ),
+    );
+  }
+}
+
+class TaskItem extends StatelessWidget {
+  const TaskItem({
+    super.key,
+    required this.taskName,
+  });
+
+  final String taskName;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: Theme.of(context).colorScheme.background,
+        ),
+        child: ListTile(
+          textColor: Theme.of(context).colorScheme.onBackground,
+          leading: Checkbox(
+            checkColor: Theme.of(context).colorScheme.secondary,
+            activeColor: Theme.of(context).colorScheme.onSecondary,
+            value: false,
+            onChanged: (value) {},
+          ),
+          title: Text(taskName),
+          trailing: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.delete),
+            color: Colors.redAccent,
+          ),
+          onTap: () {},
+        ),
       ),
     );
   }
